26d7334d9ab9ab6cc8f9b84b716b742da49e3849
Georg Svetlov <gsvetlov@gmail.com>
2021-04-09 00:42:34 +0300

    по мотивам разбора дз

--- src/main/java/ru/svetlov/multithreading/MainApp.java
+++ src/main/java/ru/svetlov/multithreading/MainApp.java
@@ -6,13 +6,13 @@ import java.util.concurrent.CyclicBarrier;
 
 public class MainApp {
     public static final int CARS_COUNT = 4;
-    private static final CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT + 1);
-    private static final CountDownLatch startSignal = new CountDownLatch(1);
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT + 1);
+        CountDownLatch startSignal = new CountDownLatch(1);
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrier, startSignal);
         }
